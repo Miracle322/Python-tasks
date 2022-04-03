b754c88ae62eab00f61011e9455ebcb18fe12494
Miracle322 <102928036+Miracle322@users.noreply.github.com>
2022-04-04 00:52:02 +0300

    Create max_sub_sequence_ones.py

---
+++ max_sub_sequence_ones.py
@@ -0,0 +1,17 @@
+def max_sub_sequence_ones(array: list) -> int:
+    """Дан массив из нулей и единиц. Нужно определить, какой максимальный по длине подинтервал единиц можно получить,
+       удалив ровно один элемент массива."""
+    max_length, left_count_ones, right_count_ones, intermediate_zero = 0, 0, 0, False
+    for el in array:
+        if el == 1:
+            left_count_ones += 1
+            right_count_ones += 1
+        elif el == 0 and intermediate_zero is True:
+            max_length = max(max_length, left_count_ones)
+            left_count_ones = 0
+            intermediate_zero = False
+        elif el == 0 and intermediate_zero is False:
+            max_length = max(max_length, right_count_ones)
+            right_count_ones = 0
+            intermediate_zero = True
+    return max(max_length, max(left_count_ones, right_count_ones))
