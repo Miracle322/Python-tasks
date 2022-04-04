44b23f4be0ad5614b43601d05a15e005bcd4e940
Miracle322 <102928036+Miracle322@users.noreply.github.com>
2022-04-04 15:09:56 +0300

    Update merge.py

--- merge.py
+++ merge.py
@@ -14,3 +14,21 @@ def merge(ranges: list) -> list:
             start, end = i[0], i[1]
     result.append([start, end])
     return result
+
+
+def main():
+    result = merge([[1, 3], [100, 200], [2, 4]])
+    assert result == [[1, 4], [100, 200]], f'Wrong answer: {result}'
+    result = merge([])
+    assert result == [], f'Wrong answer: {result}'
+    result = merge([[1, 8], [100, 200], [10, 40], [20, 30], [30, 60], [50, 90]])
+    assert result == [[1, 8], [10, 90], [100, 200]], f'Wrong answer: {result}'
+    result = merge([[1, 8], [100, 200], [10, 40]])
+    assert result == [[1, 8], [10, 40], [100, 200]], f'Wrong answer: {result}'
+    result = merge([[1, 8], [100, 200], [10, 40], [30, 60], [150, 220]])
+    assert result == [[1, 8], [10, 60], [100, 220]], f'Wrong answer: {result}'
+    print('Done')
+
+
+if __name__ == '__main__':
+    main()
