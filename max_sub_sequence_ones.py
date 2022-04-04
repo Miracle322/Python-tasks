04ab2f7d20482fe12c99d8c57b84e24d77f566ec
Miracle322 <102928036+Miracle322@users.noreply.github.com>
2022-04-04 14:39:14 +0300

    Update max_sub_sequence_ones.py

--- max_sub_sequence_ones.py
+++ max_sub_sequence_ones.py
@@ -15,3 +15,21 @@ def max_sub_sequence_ones(array: list) -> int:
             right_count_ones = 0
             intermediate_zero = True
     return max(max_length, max(left_count_ones, right_count_ones))
+
+
+def main():
+    result = max_sub_sequence_ones([0, 1, 1, 1, 0])
+    assert result == 3, f'Wrong answer: {result}'
+    result = max_sub_sequence_ones([])
+    assert result == 0, f'Wrong answer: {result}'
+    result = max_sub_sequence_ones([0, 0, 0, 0, 0])
+    assert result == 0, f'Wrong answer: {result}'
+    result = max_sub_sequence_ones([1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0])
+    assert result == 6, f'Wrong answer: {result}'
+    result = max_sub_sequence_ones([1, 1, 0, 1])
+    assert result == 3, f'Wrong answer: {result}'
+    print('Done')
+
+
+if __name__ == '__main__':
+    main()
