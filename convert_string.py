9d8e6fec0d166a884d5945c8ccdc560e4795904e
Miracle322 <102928036+Miracle322@users.noreply.github.com>
2022-04-04 14:17:32 +0300

    Update convert_string.py

--- convert_string.py
+++ convert_string.py
@@ -13,3 +13,19 @@ def convert(s: str) -> str:
                 result.append(word + str(count))
             count = 1
     return ''.join(result)
+
+
+def main():
+    result = convert('AAAABBBCCXYZDDDDEEEFFFAAAAAABBBBBBBBBBBBBBBBBBBBBBBBBBBB')
+    assert result == 'A4B3C2XYZD4E3F3A6B28', f'Wrong answer: {result}'
+    result = convert('ABCDE')
+    assert result == 'ABCDE', f'Wrong answer: {result}'
+    result = convert('')
+    assert result == '', f'Wrong answer: {result}'
+    result = convert('EEECDLAA')
+    assert result == 'E3CDLA2', f'Wrong answer: {result}'
+    print('Done')
+
+
+if __name__ == '__main__':
+    main()
